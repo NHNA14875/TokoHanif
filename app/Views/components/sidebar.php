38fd61eb549ac335105bb4cc9b4a767240719b44
NHNA14875 <kohahong426@gmail.com>
2025-06-30 23:03:57 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -26,6 +26,13 @@
                     <span>Produk</span>
                 </a>
             </li><!-- End Produk Nav -->
+            
+            <li class="nav-item">
+    <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+        <i class="bi bi-person"></i>
+        <span>Profile</span>
+    </a>
+</li><!-- End Profile Nav -->
 
             <li class="nav-item">
                 <a class="nav-link <?php echo (uri_string() == 'kategori-produk') ? "" : "collapsed" ?>" href="kategori-produk">
